10dc546d2d90c26224aa1dc450d3d01bbf7a6c3d
Adrian Brunninger <adrian.brunninger@gmail.com>
2021-03-17 18:29:19 +0100

    add ui to test api calls

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,89 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h2 class="text-left">Test REST-API</h2>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col cols="6">
+        <h4 class="text-left">Enter a URL</h4>
+      </v-col>
+      <v-col cols="6">
+        <h4 class="text-left">Choose Request-Method</h4>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col cols="6">
+        <v-text-field type="text" label="URL" v-model="url"/>
+      </v-col>
+      <v-col cols="6">
+        <v-select :items="methods" v-model="method" label="Request-Method"/>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h4 class="text-left">Enter JSON-Body</h4>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-textarea type="text" label="JSON" v-model="json"/>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-btn class="float-right" color="primary" type="submit" @click="doRequest">do request</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h4 class="text-left">Response</h4>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <span>{{response}}</span>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import axios, { AxiosResponse } from 'axios'
 
 export default Vue.extend({
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data: () => ({
+    url: '',
+    json: '',
+    method: '',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    response: ''
+  }),
+
+  methods: {
+    doRequest: function () {
+      if (this.method === 'GET') {
+        axios.get(this.url).then((response: AxiosResponse) => {
+          this.response = response.data
+        })
+      } else if (this.method === 'POST') {
+        axios.post(this.url, this.json, { headers: { 'Content-Type': 'application/json' } }).then((response: AxiosResponse) => {
+          this.response = response.data
+        })
+      } else if (this.method === 'PUT') {
+        axios.put(this.url, this.json, { headers: { 'Content-Type': 'application/json' } }).then((response: AxiosResponse) => {
+          this.response = response.data
+        })
+      } else {
+        axios.delete(this.url, { headers: { 'Content-Type': 'application/json' } }).then((response: AxiosResponse) => {
+          this.response = response.data
+        })
+      }
+    }
   }
 })
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
